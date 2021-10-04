8cbd8887320ffd6afda21d6ce082573efc32343a
Arif870 <arifuzzaman959@gmail.com>
2021-10-04 11:48:10 +0600

    contact page done

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,5 +1,31 @@
 import React from "react";
+import { Form, Button } from "react-bootstrap";
+import "./Contact.css";
 
 export default function Contact() {
-  return <div>This is Contact component</div>;
+  return (
+    <div className="contact text-center mt-5">
+      <h1>Contact Us</h1>
+      <p>Have any questions ? we'd love to hear from you.</p>
+
+      <div className="container w-50 mt-5">
+        <Form>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Control type="email" placeholder="@gmail.com" />
+          </Form.Group>
+          <Form.Group
+            className="mb-3 text-start"
+            controlId="exampleForm.ControlTextarea1"
+          >
+            <Form.Control
+              as="textarea"
+              rows={3}
+              placeholder="Give us your valuable feedback"
+            />
+          </Form.Group>
+        </Form>
+        <Button>Submit</Button>
+      </div>
+    </div>
+  );
 }
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Home() {
-  return <div>This is home component</div>;
+  return <div>This is home page</div>;
 }
